d03f456611ddb6b1b962aaa2cdf4666a94bf4e78
Ben Krocke <benkrocke@gmail.com>
2023-11-26 18:07:36 +1100

    Refactor the file

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
 #[macro_use]
 extern crate serde_derive;
 
-use reqwest::Url;
 use reqwest::blocking::Client;
+use reqwest::Url;
 use serde_bencode::de;
 use serde_bencode::ser;
 use serde_bencode::value::Value;
 use serde_bytes::ByteBuf;
 use sha1::{Digest, Sha1};
+use std::fs::File as FsFile;
 use std::io::Read;
-use std::{collections::HashMap, fs::File as FsFile};
 use urlencoding::encode;
-use urlencoding::encode_binary;
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Node(String, i64);
@@ -100,85 +99,75 @@ fn render_torrent(torrent: &Torrent) {
     }
 }
 
-fn initial_request(torrent: &Torrent) -> Result<(), Box<dyn std::error::Error>> {
-    //render_torrent(&torrent);
-    let announce = torrent.announce.as_ref().unwrap();
-
-    let info_hash = (&torrent.info_hash).to_string();
+fn escape_info_hash(info_hash: &str) -> String {
     let zipped = info_hash.chars().zip(info_hash.chars().skip(1));
 
     let mut escaped_info_hash = String::new();
-    
+
     for (hex1, hex2) in zipped.step_by(2) {
-	let new_str = format!("%{hex1}{hex2}");
-	escaped_info_hash.push_str(new_str.as_str());
+        let new_str = format!("%{hex1}{hex2}");
+        escaped_info_hash.push_str(new_str.as_str());
     }
-    
+
+    escaped_info_hash
+}
+
+fn initial_request(torrent: &Torrent) -> Result<(), Box<dyn std::error::Error>> {
+    let announce = torrent.announce.as_ref().unwrap();
+
+    let info_hash = escape_info_hash(torrent.info_hash.as_str());
     let params = [
-        ("peer_id", encode("makethisuniqueplease").to_string()),
+        ("peer_id", encode("makethisuniqueplease").to_string()), // TODO: Better id
         ("port", "6881".to_string()),
         ("uploaded", "0".to_string()),
         ("downloaded", "0".to_string()),
-        ("left", "524288".to_string()),
+        ("left", torrent.info.piece_length.to_string()),
         ("compact", "1".to_string()),
         ("event", "started".to_string()),
     ];
-    
+
     let client = Client::new();
     let mut url = Url::parse_with_params(announce, &params)?;
-    url.set_query(Some(format!("info_hash={escaped_info_hash}").as_str()));
-    let response = client.get(url).send().unwrap().text().unwrap();
+    url.set_query(Some(format!("info_hash={info_hash}").as_str()));
+    let response = client.get(url).send();
 
     println!("{:#?}", response);
     Ok(())
 }
 
-fn start(torrent: &Torrent) {
-    //render_torrent(&torrent);
-    match &torrent.announce {
-        Some(a) => match initial_request(torrent) {
-            Ok(()) => println!("Success"),
-            Err(e) => println!("{}", e),
-        },
-        None => println!("No announce URL, aborting program."),
-    }
-    return;
-}
-
-fn get_info_hash(buffer: &Vec<u8>) -> Result<String, Box<dyn std::error::Error>> {
+fn get_info_hash(buffer: &Vec<u8>) -> String {
     #[derive(Deserialize)]
     struct MetaInfo {
         info: Value,
     }
 
-    let meta_info = de::from_bytes::<MetaInfo>(&buffer)?;
-    let info = ser::to_bytes(&meta_info.info)?;
+    let meta_info = de::from_bytes::<MetaInfo>(&buffer).expect("Torrent file could not be parsed.");
+    let info = ser::to_bytes(&meta_info.info).expect("Info could not be serialized.");
     let mut hasher = Sha1::new();
     hasher.update(&info);
     let info_hash = hasher.finalize();
-    Ok(hex::encode(&info_hash).to_string())
+    hex::encode(&info_hash).to_string()
+}
+
+fn load_file_into_buffer() -> Vec<u8> {
+    let mut file = FsFile::open("/home/benkrocke/code/rust/bittorrent/tor.torrent")
+        .expect("Torrent file could not be found.");
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer).unwrap();
+    buffer
+}
+
+fn parse_buffer(buffer: Vec<u8>) -> Torrent {
+    let mut torrent =
+        de::from_bytes::<Torrent>(&buffer).expect("Torrent file could not be parsed.");
+    let info_hash = get_info_hash(&buffer);
+    torrent.info_hash = info_hash;
+    torrent
 }
 
 fn main() {
-    match FsFile::open("/home/benkrocke/code/rust/bittorrent/tor.torrent") {
-        Ok(mut f) => {
-            let mut buffer = Vec::new();
-            match f.read_to_end(&mut buffer) {
-                Ok(_) => match de::from_bytes::<Torrent>(&buffer) {
-                    Ok(mut t) => {
-                        match get_info_hash(&buffer) {
-			    Ok(info_hash) => {
-				t.info_hash = info_hash;
-				start(&t);
-			    },
-			    Err(e) => println!("ERROR: {e:?}")
-			}
-                    }
-                    Err(e) => println!("ERROR: {e:?}"),
-                },
-                Err(e) => println!("ERROR: {e:?}"),
-            }
-        }
-        Err(e) => println!("ERROR: {e:?}"),
-    }
+    let buffer = load_file_into_buffer();
+    let torrent = parse_buffer(buffer);
+    
+    initial_request(&torrent).unwrap();
 }
